37d66b37437fdfd1d6a148d583575f71637917bd
Jawwad <mjawwad072@gmail.com>
2022-03-28 00:03:05 +0500

    checkpoint before websockets

--- app.js
+++ app.js
@@ -27,30 +27,6 @@ router.use((req, res, next) =>
     next();
 });
 
-
-router.route('/user').post(JsonParser, (req, res) =>
-{
-    //console.log(req.params.username);
-    let u = {...req.body};
-
-    dboperations.authenticateUser(u.username, u.password).then(result =>
-    {
-        //res.json(result[0].length);
-        if (result[0].length === 1)
-        {
-            res.status(200).json(result[0]);
-        }
-        else if (result[0].length > 1)
-        {
-            res.status(400).json("Bad Request");
-        }
-        else
-        {
-            res.status(403).send("Forbidden");
-        }
-    })
-});
-
 router.route('/users').get((req, res) =>
 {
     dboperations.getEmployees().then(result => 
@@ -66,6 +42,39 @@ router.route('/users').get((req, res) =>
     });
 });
 
+router.route('/user').post(JsonParser, (req, res) =>
+{
+    //console.log(req.params.username);
+    let u = {...req.body};
+    //console.log(u);
+
+    dboperations.authenticateUser(u.username, u.password, u.usertype).then(result =>
+    {
+        //res.json(result[0].length);
+        if (result[0].length === 1)
+        {
+            //console.log("Hello");
+            res.status(200).json(result[0]);
+        }
+        else if (result[0].length > 1)
+        {
+            res.status(400).json("Bad Request");
+        }
+        else
+        {
+            res.status(403).send("Forbidden");
+        }
+    })
+});
+
+router.route('/user/:id').get(JsonParser, (request, res) => 
+{
+    dboperations.getUserById(request.params.id).then( result => 
+    {
+        res.status(200).json(result);
+    });
+});
+
 router.route('/users').post(JsonParser, (request, res) => 
 {
     let e = {...request.body};
@@ -77,15 +86,75 @@ router.route('/users').post(JsonParser, (request, res) =>
     });
 });
 
+router.route('/users/:id').put(JsonParser, (request, res) => 
+{
+    let u = {...request.body};
+    //console.log(u);
+
+    dboperations.updateEmployee(request.params.id, u).then( result => 
+    {
+        res.status(201).json(result);
+    });
+});
+
 router.route('/menu').get(JsonParser, (req, res) =>
 {
     //let p = {...req.body};
     let p = {...req.query};
     //let y = {...req.query.params.category}
     //console.log(p);
-    //console.log(req.query.category);
+    console.log(req.query.category);
 
     dboperations.getMenuItems(p.category).then(result => 
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/menu/:id').get(JsonParser, (req, res) =>
+{
+    dboperations.getMenuById(req.params.id).then(result => 
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/menu/:id').put(JsonParser, (req, res) =>
+{
+    let m = {...req.body};
+
+    dboperations.updateMenu(req.params.id, m).then(result => 
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/menu').post(JsonParser, (req, res) =>
+{
+    //FIRST FETCH CATEGORY API
+    //THEN FETCH ITEM CATEGORY API
+
+    let m = {...req.body};
+
+    console.log(m);
+
+    dboperations.addMenu(m).then(result => 
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/amenu').get(JsonParser, (req, res) =>
+{
+    dboperations.getItems().then(result =>
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/items/:id').get(JsonParser, (req, res) =>
+{
+    dboperations.getItemsById(req.params.id).then(result =>
     {
         res.status(200).send(result);
     });
@@ -118,6 +187,119 @@ router.route('/order').post(JsonParser, (req, res) =>
     });
 });
 
+router.route('/cheforders').get(JsonParser, (req, res) =>
+{
+    let o = {...req.query};
+
+    console.log(o);
+    dboperations.getChefOrders(o.chef_id, o.order_status).then(result =>
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/lookfororder').get(JsonParser, (req, res) =>
+{
+    dboperations.getAnOrder().then(result =>
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/assignorder').get(JsonParser, (req, res) =>
+{
+    let o = {...req.query};
+
+    console.log("Test point 1")
+    console.log(o);
+
+    dboperations.assignOrder(o.order_id, o.chef_id, o.order_status, o.est_time, o.accepted_time).then(result =>
+    {
+        console.log("Test Point 6");
+        console.log(result);
+        if (result.length === 1)
+        {
+            res.status(200).send(result);
+        }
+        else
+        {
+            res.status(304).send('Hello');
+        }
+    });
+});
+
+router.route('/changeorderstatus').get(JsonParser, (req, res) =>
+{
+    let o = {...req.query};
+
+    console.log(o)
+    dboperations.changeOrderStatus(o.order_id, o.chef_id, o.order_status).then(result =>
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/changetablestatus').get(JsonParser, (req, res) =>
+{
+    let t = {...req.query};
+
+    console.log(t)
+    dboperations.changeTableStatus(t.table_id, t.table_status).then(result =>
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/inventory').get(JsonParser, (req, res) =>
+{
+    dboperations.getInventories().then(result =>
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/inventory/:id').get(JsonParser, (req, res) =>
+{
+
+    console.log("HEllo");
+    dboperations.getInventoryById(req.params.id).then(result =>
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/inventory').post(JsonParser, (req, res) =>
+{
+    //FIRST FETCH INVENTOY CATEGORIES
+
+    let i = {...req.body};
+
+    //console.log(i);
+
+    dboperations.addInventory(i).then(result =>
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/inventory/:id').put(JsonParser, (req, res) =>
+{
+    let i = {...req.body}
+
+    dboperations.updateInventory(req.params.id, i).then(result =>
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/icategory').get(JsonParser, (req, res) =>
+{
+    dboperations.getInventoryCategories().then(result =>
+    {
+        res.status(200).send(result);
+    });
+});
+
 router.route('/tables').get(JsonParser, (req, res) =>
 {
     dboperations.getTables().then( result =>
@@ -134,17 +316,41 @@ router.route('/image/:id').get( (req, res) =>
     });
 });
 
+router.route('/badreviews').get(JsonParser, (req, res) =>
+{
+    dboperations.getBadReviews().then( result =>
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/foodcategories').get(JsonParser, (req, res) =>
+{
+    dboperations.getFoodCategories().then( result =>
+    {
+        res.status(200).send(result);
+    });
+});
+
+router.route('/itemcategories').get(JsonParser, (req, res) =>
+{
+    dboperations.getItemCategories().then( result =>
+    {
+        res.status(200).send(result);
+    });
+});
+
 //PORT
-/*
+
 const port = process.env.PORT || 3000;
-app.listen(port, '172.16.69.28', function()
+app.listen(port, '192.168.10.4', function()
 {
     console.log(`Server Started on port ${port}...`);
 });
-*/
 
-const port = process.env.PORT || 3000;
-app.listen(port, '0.0.0.0', function()
-{
-    //console.log(`Server Started on port ${port}...`);
-});
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, '0.0.0.0', function()
+// {
+//     //console.log(`Server Started on port ${port}...`);
+// });
--- dboperations.js
+++ dboperations.js
@@ -1,15 +1,132 @@
 var config = require('./dbconfig');
 const sql = require('mssql');
 const Modules = require('./modules');
+const { contentDisposition } = require('express/lib/utils');
 
 async function getOrders ()
 {
     try
     {
         let pool = await sql.connect(config);
-        let orders = await pool.request().query("SELECT * FROM ORDERS");
+        let orders = await pool.request().query("SELECT * FROM ORDERS INNER JOIN USER_ ON ORDERS.U_id=USER_.u_id WHERE USER_.usertype <> 'admin'");
         pool.close();
-        return orders.recordsets;
+        return orders.recordsets[0];
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+async function getAnOrder ()
+{
+    try
+    {
+        let pool = await sql.connect(config);
+        let order = await pool.request().query(`SELECT order_id, order_time, total_price, table_id FROM ORDERS INNER JOIN USER_ ON ORDERS.U_id=USER_.u_id WHERE USER_.usertype='admin'`);
+        pool.close();
+        let incomming_orders = order.recordsets[0]
+        for (let single of incomming_orders)
+        {
+            let itt = await getOrderItems (single.order_id);
+            single["items"] = itt;
+        }
+
+        console.log(incomming_orders);
+        return incomming_orders;
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+async function assignOrder (order_id, chef_id, order_status, est_time, accepted_time)
+{
+    try
+    {
+        let pool = await sql.connect(config);
+        let check = await pool.request().query(`SELECT * FROM orders WHERE order_id=${order_id}`);
+        pool.close();
+        console.log("Test Point 2");
+        console.log(check.recordset);
+        let c_id = check.recordset[0].u_id;
+        console.log("Test Point 3")
+        console.log(c_id)
+        if (c_id === 1)
+        {
+            let pool2 = await sql.connect(config);
+            let order = await pool2.request().query(`UPDATE ORDERS SET order_status='${order_status}', u_id=${chef_id}, est_time='${est_time}', accepted_time='${accepted_time}' WHERE order_id=${order_id}`);
+            pool2.close();
+            console.log("Test Point 4")
+            console.log(order.recordset);
+            let pool1 = await sql.connect(config);
+            let final = await pool1.request().query(`SELECT * FROM ORDERS WHERE order_status='${order_status}' AND u_id=${chef_id} AND order_id=${order_id}`);
+            console.log("Test Point 5")
+            console.log(final.recordset);
+            pool1.close();
+            return final.recordset;
+        }
+        else
+        {
+            return [];
+        }
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+async function changeOrderStatus (order_id, chef_id, order_status)
+{
+    try
+    {
+        let pool = await sql.connect(config);
+        let order = await pool.request().query(`UPDATE ORDERS SET order_status='${order_status}', u_id=${chef_id} WHERE order_id=${order_id}`);
+        pool.close();
+        return order.recordset;
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+async function changeTableStatus (table_id, table_status)
+{
+    try
+    {
+        let pool = await sql.connect(config);
+        let order = await pool.request().query(`UPDATE tables SET table_status='${table_status}' WHERE order_id=${table_id}`);
+        pool.close();
+        return order.recordset;
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+async function getChefOrders (chef_id, order_status)
+{
+    try
+    {
+        let pool = await sql.connect(config);
+        let orders = await pool.request().query(`SELECT * FROM ORDERS WHERE u_id = ${chef_id} AND order_status = '${order_status}'`);
+        let result = orders.recordsets[0];
+        pool.close();
+        for (let each of result)
+        {
+            //console.log(each.order_id);
+            //console.log(each.order_id);
+            //console.log(each);
+            let temp = await getOrderItems(each.order_id);
+            each["items"] = temp;
+            //console.log(temp);
+            //console.log(each);
+        }
+        return orders.recordsets[0];
     }
     catch (error)
     {
@@ -29,11 +146,17 @@ async function getOrderItems (id)
         output = orders.recordset;
         for (let item of oitems) 
         {
+            //console.log("Hello");
+            //console.log(item);
+
             let ind = await getItemsById(item.item_id);
+            //console.log(ind);
             item["item_name"] = ind.item_name;
+            //item["item_details"] = ind;
         }
 
-        console.log(oitems);
+        //console.log(oitems);
+        //console.log(orders.recordsets[0]);
         
         return orders.recordsets[0];
     }
@@ -49,6 +172,21 @@ async function getUsers ()
     {
         let pool = await sql.connect(config);
         let orders = await pool.request().query("SELECT * FROM USER_");
+        pool.close();
+        return orders.recordsets[0];
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+async function getUserById (id)
+{
+    try
+    {
+        let pool = await sql.connect(config);
+        let orders = await pool.request().query(`SELECT * FROM USER_ WHERE u_id=${id}`);
         pool.close();
         return orders.recordsets[0];
     }
@@ -92,12 +230,30 @@ async function addEmployee (employee)
     }
 }
 
-async function authenticateUser (username, password)
+async function updateEmployee (id, u)
 {
     try
     {
         let pool = await sql.connect(config);
-        let orders = await pool.request().query(`SELECT * FROM USER_ WHERE username = '${username}' AND password = '${password}'`);
+        let orders = await pool.request().query(`UPDATE USER_ SET phone_no=${u.phone_no}, 
+                    password='${u.password}', usertype='${u.usertype}', 
+                    NIC=${u.NIC}, username='${u.username}', 
+                    users_name='${u.users_name}' WHERE u_id=${id}`);
+        pool.close();
+        return orders.recordsets;
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+async function authenticateUser (username, password, usertype)
+{
+    try
+    {
+        let pool = await sql.connect(config);
+        let orders = await pool.request().query(`SELECT * FROM USER_ WHERE username = '${username}' AND password = '${password}' AND usertype = '${usertype}'`);
         pool.close();
         return orders.recordsets;
     }
@@ -114,22 +270,7 @@ async function getFoodCategories ()
         let pool = await sql.connect(config);
         let orders = await pool.request().query("SELECT * FROM Food_category");
         pool.close();
-        return orders.recordsets;
-    }
-    catch (error)
-    {
-        console.log(error);
-    }
-}
-
-async function getInventoryCategories ()
-{
-    try
-    {
-        let pool = await sql.connect(config);
-        let orders = await pool.request().query("SELECT * FROM Inventory_category");
-        pool.close();
-        return orders.recordsets;
+        return orders.recordsets[0];
     }
     catch (error)
     {
@@ -174,7 +315,7 @@ async function getTables ()
         let pool = await sql.connect(config);
         let orders = await pool.request().query("SELECT * FROM tables");
         pool.close();
-        return orders.recordsets;
+        return orders.recordsets[0];
     }
     catch (error)
     {
@@ -182,14 +323,78 @@ async function getTables ()
     }
 }
 
-
-
 async function getInventories ()
 {
     try
     {
         let pool = await sql.connect(config);
-        let orders = await pool.request().query("SELECT * FROM inventory");
+        let orders = await pool.request().query("SELECT * FROM inventory INNER JOIN inventory_category ON inventory.category_id=inventory_category.category_id");
+        pool.close();
+        return orders.recordsets[0];
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+async function getInventoryById (id)
+{
+    try
+    {
+        let pool = await sql.connect(config);
+        let orders = await pool.request().query(`SELECT * FROM inventory WHERE inventory_id=${id}`);
+        pool.close();
+        return orders.recordsets[0];
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+
+}
+
+async function getInventoryCategories ()
+{
+    try
+    {
+        let pool = await sql.connect(config);
+        let orders = await pool.request().query("SELECT * FROM Inventory_category");
+        pool.close();
+        return orders.recordsets[0];
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+async function addInventory (inventory)
+{
+    try
+    {
+        let pool = await sql.connect(config);
+        let orders = await pool.request().query(`INSERT INTO inventory (inventory_name, date_of_purchase, cost, quantity, total_cost, category_id)
+        VALUES ('${inventory.inventory_name}', '${inventory.date_of_purchase}', '${inventory.cost}', '${inventory.quantity}', 
+                '${inventory.total_cost}', '${inventory.category_id}')`);
+        pool.close();
+        return orders.recordsets[0];
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+async function updateInventory (id, i)
+{
+    try
+    {
+        let pool = await sql.connect(config);
+        let orders = await pool.request().query(`UPDATE inventory SET inventory_name='${i.inventory_name}', 
+                    date_of_purchase='${i.date_of_purchase}', cost=${i.cost}, 
+                    quantity=${i.quantity}, total_cost=${i.total_cost}, 
+                    category_id=${i.category_id} WHERE inventory_id=${id}`);
         pool.close();
         return orders.recordsets;
     }
@@ -206,7 +411,7 @@ async function getItemCategories ()
         let pool = await sql.connect(config);
         let orders = await pool.request().query("SELECT * FROM item_category");
         pool.close();
-        return orders.recordsets;
+        return orders.recordsets[0];
     }
     catch (error)
     {
@@ -221,7 +426,7 @@ async function getItems ()
         let pool = await sql.connect(config);
         let orders = await pool.request().query("SELECT * FROM items");
         pool.close();
-        return orders.recordsets;
+        return orders.recordsets[0];
     }
     catch (error)
     {
@@ -237,6 +442,42 @@ async function getItemsById (id)
         let orders = await pool.request().query(`SELECT item_name FROM items WHERE item_id = ${id} `);
         pool.close();
         return orders.recordsets[0][0];
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+async function getMenuById (id)
+{
+    try
+    {
+        let pool = await sql.connect(config);
+        let orders = await pool.request().query(`SELECT * FROM items WHERE item_id = ${id} `);
+        pool.close();
+        return orders.recordset;
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+async function updateMenu (id, m)
+{
+    console.log(m);
+
+    try
+    {
+        let pool = await sql.connect(config);
+        let orders = await pool.request().query(`UPDATE items SET item_name='${m.item_name}', 
+                    item_price=${m.item_price}, item_description='${m.item_description}', 
+                    menu_status='${m.menu_status}', image_id=${m.image_id}, 
+                    category_id=${m.category_id}, icategory_id=${m.icategory_id}    
+                    WHERE item_id=${id}`);
+        pool.close();
+        return orders.recordset;
     }
     catch (error)
     {
@@ -283,6 +524,23 @@ async function getMenuItems (fcat)
         //console.log(JsonString);
         pool.close();
         return JsonString;
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+async function addMenu (m)
+{
+    try
+    {
+        let pool = await sql.connect(config);
+        let newProduct = await pool.request()
+        .query(`INSERT INTO items (item_name, item_price, item_description, menu_status, image_id, category_id, icategory_id)
+                VALUES ('${m.item_name}', ${m.item_price}, '${m.item_description}', '${m.menu_status}', ${m.image_id}, ${m.category_id}, ${m.icategory_id})`);
+        pool.close();
+        return newProduct.recordsets;
     }
     catch (error)
     {
@@ -350,6 +608,21 @@ async function processOrder (o)
     }
 }
 
+async function getBadReviews ()
+{
+    try
+    {
+        let pool = await sql.connect(config);
+        let orders = await pool.request().query("SELECT total_price, payment_mode, rating, table_id, users_name FROM ORDERS INNER JOIN USER_ ON ORDERS.U_id=USER_.u_id WHERE ORDERS.rating < 3");
+        pool.close();
+        return orders.recordsets[0];
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
 module.exports = 
 {
     getOrders : getOrders,
@@ -369,5 +642,20 @@ module.exports =
     getMenuItems : getMenuItems,
     addOrder : addOrder,
     addOrdersItems : addOrdersItems,
-    processOrder : processOrder
+    processOrder : processOrder,
+    getChefOrders : getChefOrders,
+    getAnOrder : getAnOrder,
+    changeOrderStatus : changeOrderStatus,
+    addInventory : addInventory,
+    addMenu : addMenu,
+    getMenuById : getMenuById,
+    updateMenu : updateMenu,
+    updateEmployee : updateEmployee,
+    getInventoryById : getInventoryById,
+    updateInventory : updateInventory,
+    getItemsById : getItemsById,
+    assignOrder : assignOrder,
+    getBadReviews : getBadReviews,
+    getUserById : getUserById,
+    changeTableStatus : changeTableStatus
 }
